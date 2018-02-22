8de053c5ce926364320e3f7001b5fc3bbdfd1816
andreacuellar2 <cuellarandrea@gmail.com>
2018-02-22 01:03:44 -0600

    clase objeto para las columnas

---
+++ src/main/java/objetos/Objeto.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author andrea
+ */
+public class Objeto {
+    
+    
+    private String colum1;
+    private String colum2;
+    private String colum3;
+    private String colum4;
+
+    /**
+     * @return the colum1
+     */
+    public String getColum1() {
+        return colum1;
+    }
+
+    /**
+     * @param colum1 the colum1 to set
+     */
+    public void setColum1(String colum1) {
+        this.colum1 = colum1;
+    }
+
+    /**
+     * @return the colum2
+     */
+    public String getColum2() {
+        return colum2;
+    }
+
+    /**
+     * @param colum2 the colum2 to set
+     */
+    public void setColum2(String colum2) {
+        this.colum2 = colum2;
+    }
+
+    /**
+     * @return the colum3
+     */
+    public String getColum3() {
+        return colum3;
+    }
+
+    /**
+     * @param colum3 the colum3 to set
+     */
+    public void setColum3(String colum3) {
+        this.colum3 = colum3;
+    }
+
+    /**
+     * @return the colum4
+     */
+    public String getColum4() {
+        return colum4;
+    }
+
+    /**
+     * @param colum4 the colum4 to set
+     */
+    public void setColum4(String colum4) {
+        this.colum4 = colum4;
+    }
+    
+    
+    
+}
